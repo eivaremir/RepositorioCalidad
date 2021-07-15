70c37ed779050b85b3169bf67cd7a9e8407cace2
Eivar Morales <eivaremir@gmail.com>
2021-07-15 18:30:14 +0000

    p4

---
+++ untitled.java
@@ -0,0 +1,10 @@
+public static boolean esPrimo(int numero){
+    int contador = 2;
+    boolean primo = true;
+    while((primo) && (contador !=numero)){
+    if(n%c == 0)
+        primo = false
+    contador++
+    }
+    return primo
+}
---
+++ untitled2.java
@@ -0,0 +1,10 @@
+public static void burbuja(int[] matrix){
+int t;
+for(int i=1; i<m.l;i++){
+for(int j=0; j<m.l-1;j++){
+if(matrix[j]>matrix[j+4]){
+temp=1
+}
+}
+}
+}
