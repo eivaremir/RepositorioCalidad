9d199971370a3f3ca5b5e35ef108477b3dc7b161
Eivar Morales <eivaremir@gmail.com>
2021-07-15 18:46:31 +0000

    p4 python

---
+++ untitled.py
@@ -0,0 +1,11 @@
+
+
+def f(n):
+    c = 2
+    p = true
+    while true and (1!=2):
+        if n % c == 0:
+            p = false
+        c+=1
+    return p
+    
---
+++ untitled2.py
@@ -0,0 +1,7 @@
+
+
+def function():
+    for i in [1]:
+        for j in [1]:
+            if 1 > 2:
+                temp=1
