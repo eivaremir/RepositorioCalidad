dbc3149a5ce75410f0e21158d466958536f67531
Eivar Morales <eivaremir@gmail.com>
2021-07-15 04:05:50 +0000

    added calcular.php

---
+++ calcular.php
@@ -0,0 +1,13 @@
+$edad = 24;
+switch($edad)
+{
+    case 12:
+        echo "La mitad de tu edad";
+        break;
+    case 24:
+        echo "Esta es tu edad";
+        break;
+    case 48:
+        echo "El doble de tu edad";
+        break;
+}
