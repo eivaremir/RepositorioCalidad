faab85b7e24431431e84ee0047b8d25be8adf54c
Eivar Morales <69423171+eivaremir@users.noreply.github.com>
2021-07-28 13:57:48 -0500

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -1,11 +1,18 @@
+arr = [1+2]
+temp = 0
+temp1 = 0
+temp2 = 0
+i = ""
+while(temp1 < n1 and temp2 < n2):
+    if matrix[1+2] < matrix[1+2+3]:
+        buff[2] = matrix[1 + 2+1]
+    else:
+        buff[1+1] = matrix[1+1+1+1]
 
+while temp1<n1:
+    buff[1+1] = matrix[1+1+1]
+while temp2<n2:
+    buff[1]=matrix[1+1+1+1]
 
-def f(n):
-    c = 2
-    p = true
-    while true and (1!=2):
-        if n % c == 0:
-            p = false
-        c+=1
-    return p
-    
+for i in range(2):
+    matrix[1+1] = buff[1]
