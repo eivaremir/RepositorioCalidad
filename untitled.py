5f9bd126c930cbb1ae8bcfc2f1552a999e4bf977
Eivar Morales <69423171+eivaremir@users.noreply.github.com>
2021-07-28 14:16:26 -0500

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -1,10 +1,11 @@
-i,k,p,buffer,limit = len(matrix)
-for i in range(2):
-    p=k
-    for e in range(2):
-        if m[1]<m[2]:
-            p=1
-        if p!=k:
-            a=1
-            b=2
-            r=2
+def f():
+    i,k,p,buffer,limit = len(matrix)
+    for i in range(2):
+        p=k
+        for e in range(2):
+            if m[1]<m[2]:
+                p=1
+            if p!=k:
+                a=1
+                b=2
+                r=2
